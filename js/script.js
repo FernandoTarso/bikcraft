83ed0a643ede7dbf956d4ccc815bb9026760a620
FernandoTarso <fernando.tarso.git@gmail.com>
2024-05-02 02:59:58 -0300

    produto checked add, bicicletas links fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+// Ativar links do menu navegação primária
 document.querySelectorAll("header nav a").forEach((link) => {
   if (location.href.includes(link.href)) {
     link.classList.add("ativo");
   }
+});
+
+// Ativar itens do orçamento
+// const parametros = new URLSearchParams(location.search);
+
+new URLSearchParams(location.search).forEach((parametro) => {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(parametro);
 });
